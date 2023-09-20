c14b9bf20d5011576569ff4de3cf03e9478c763a
yym2 <19760884+youyoumu@users.noreply.github.com>
2023-09-20 15:46:59 +0700

    Create calculator UI and basic operator function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+function add(a, b)       {return a + b;};
+function subtract(a, b)  {return a - b;};
+function multiply(a, b)  {return a * b;};
+function divide(a, b)    {return a / b;};
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    if(operator === "+") {return add(firstNumber, secondNumber);};
+    if(operator === "-") {return subtract(firstNumber, secondNumber);};
+    if(operator === "*") {return multiply(firstNumber, secondNumber);};
+    if(operator === "/") {return divide(firstNumber, secondNumber);};
+};
+
